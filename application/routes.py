6b0d8267ba46b651a8c3587d738a42db2997f5c6
Joey Perrino <epiccrashv3@gmail.com>
2022-04-16 23:57:14 -0400

    Bootstrap and base structure added

--- application/routes.py
+++ application/routes.py
@@ -7,10 +7,10 @@ import requests
 def index():
     return render_template("index.html")
 
-@app.route("/enterQualityInfo")
+@app.route("/enterQualityInfo", methods=['GET', 'POST'])
 def enterQualityInfo():
     return render_template("index.html", is_data_entered=True)
 
-@app.route("/predictQuality")
+@app.route("/predictQuality", methods=['GET', 'POST'])
 def predictQuality():
     return render_template("index.html", is_predict_quality=True)
