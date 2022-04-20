38fdb97c966391c589019ec6c1716e42c9b5bff2
Joey Perrino <epiccrashv3@gmail.com>
2022-04-19 21:49:29 -0400

    Enter quality info updated

--- application/routes.py
+++ application/routes.py
@@ -9,7 +9,38 @@ def index():
 
 @app.route("/enterQualityInfo", methods=['GET', 'POST'])
 def enterQualityInfo():
-    return render_template("index.html", is_data_entered=True)
+    # Form inputs
+    fixedAcidity = request.form.get("fixedAcidity")
+    volatileAcidity = request.form.get("volatileAcidity")
+    citricAcid = request.form.get("citricAcid")
+    freeSulfurDioxide = request.form.get("freeSulfurDioxide")
+    totalSulfurDioxide = request.form.get("totalSulfurDioxide")
+    sulfates = request.form.get("sulfates")
+    alcohol = request.form.get("alcohol")
+    wineType = request.form.get("wineType")
+    quality = request.form.get("quality")
+
+    # Convert data to JSON
+    info = json.dumps({
+        "fixedAcidity": fixedAcidity, 
+        "volatileAcidity": volatileAcidity, 
+        "citricAcid": citricAcid, 
+        "freeSulfurDioxide": freeSulfurDioxide, 
+        "totalSulfurDioxide": totalSulfurDioxide, 
+        "sulfates": sulfates, 
+        "alcohol": alcohol,
+        "wineType": wineType,
+        "quality": quality
+    })
+
+    # URL for model
+    # url = "http://localhost:3000/api/getDonatedData"
+    url = "https://wine-club-model.herokuapp.com/api/getDonatedData"
+  
+    # Post to model
+    results = requests.post(url, info)
+
+    return render_template("index.html", is_data_entered=True, fixedAcidity = fixedAcidity, volatileAcidity = volatileAcidity, citricAcid = citricAcid, freeSulfurDioxide = freeSulfurDioxide, totalSulfurDioxide = totalSulfurDioxide, sulfates = sulfates, quality = quality, alcohol = alcohol, results=results.content.decode('UTF-8'), info=info)
 
 
 @app.route("/predictQuality", methods=['GET', 'POST'])
